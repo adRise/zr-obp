e72fe24826f4f5a4cce974d1bc4e953a02f17ca0
Chenglei Niu <chenglei@tubi.tv>
2022-08-09 17:39:47 +0800

    ok

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
         "torch>=1.9.0",
         "tqdm>=4.62.2",
         "pyieoe>=0.1.1",
-        "pingouin>=0.4.0",
+        "pingouin>=0.5.0",
     ],
     license="Apache License",
     packages=find_packages(
